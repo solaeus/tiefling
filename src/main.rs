13d28844f665f29c69c52aa1c7e53bfe33951dbf
Jeff <dev@jeffa.io>
2026-07-02 04:26:37 -0400

    Reimplement basic list with custom widgets

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,20 @@
-use std::{error::Error, fs::read_dir, io};
+use std::{
+    borrow::Cow,
+    error::Error,
+    fmt::{Display, Pointer},
+    fs::{DirEntry, FileType, ReadDir, read_dir},
+    io,
+};
 
 use ratatui::{
     DefaultTerminal, Frame,
     crossterm::event::{self, KeyCode},
-    layout::{Constraint, Layout},
-    style::{Style, Stylize},
-    widgets::{List, ListState},
+    layout::{Constraint, HorizontalAlignment, Layout},
+    macros::constraints,
+    prelude::*,
+    style::Style,
+    text::{Line, Span, Text, ToSpan},
+    widgets::{List, ListItem, ListState, Widget},
 };
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -39,22 +48,107 @@ fn run(terminal: &mut DefaultTerminal) -> io::Result<()> {
 fn render(frame: &mut Frame, list_state: &mut ListState) {
     let layout = Layout::vertical([Constraint::Fill(1)]).spacing(1);
     let [area] = frame.area().layout(&layout);
+    let root_files =
+        read_dir(".").map_or_else(|_| OpenDir::empty(), |entries| OpenDir::new(entries));
+    let app = App::new(root_files);
+    let mut app_state = AppState::default();
 
-    let files = read_dir(".").map_or_else(
-        |_| Vec::new(),
-        |entries| {
-            entries
-                .map(|entry| {
-                    entry.map_or_else(
-                        |_| String::new(),
-                        |entry| entry.file_name().to_string_lossy().to_string(),
-                    )
-                })
-                .collect()
-        },
-    );
+    frame.render_stateful_widget(app, area, &mut app_state);
+}
 
-    let list = List::new(files).highlight_style(Style::default().bold());
+struct App {
+    root: OpenDir,
+}
 
-    frame.render_stateful_widget(list, area, list_state);
+impl App {
+    fn new(root: OpenDir) -> Self {
+        Self { root }
+    }
+}
+
+impl StatefulWidget for App {
+    type State = AppState;
+
+    fn render(self, area: Rect, buffer: &mut Buffer, state: &mut Self::State) {
+        self.root.render(area, buffer);
+    }
+}
+
+#[derive(Default)]
+struct AppState {
+    selected: Option<usize>,
+}
+
+struct OpenDir {
+    files: Vec<FileListing>,
+}
+
+impl OpenDir {
+    fn new(dir: ReadDir) -> Self {
+        let mut files = Vec::new();
+
+        for entry in dir {
+            let listing = match entry {
+                Ok(entry) => FileListing::new(entry, false),
+                Err(_) => FileListing::Error,
+            };
+
+            files.push(listing);
+        }
+
+        Self { files }
+    }
+
+    fn empty() -> Self {
+        Self { files: Vec::new() }
+    }
+}
+
+impl Widget for OpenDir {
+    fn render(self, area: Rect, buffer: &mut Buffer) {
+        let constraints = vec![Constraint::Length(1); self.files.len()];
+        let areas = area.layout_vec(&Layout::vertical(constraints));
+
+        for (file, area) in self.files.into_iter().zip(areas) {
+            file.render(area, buffer);
+        }
+    }
+}
+
+enum FileListing {
+    Selected(String),
+    Unselected(String),
+    OpenDir(OpenDir),
+    Error,
+}
+
+impl FileListing {
+    fn new(entry: DirEntry, selected: bool) -> Self {
+        match entry.file_type() {
+            Ok(_) => {
+                let name = entry.file_name().to_string_lossy().to_string();
+
+                if selected {
+                    Self::Selected(name)
+                } else {
+                    Self::Unselected(name)
+                }
+            }
+            _ => Self::Error,
+        }
+    }
+}
+
+impl Widget for FileListing {
+    fn render(self, area: Rect, buffer: &mut Buffer)
+    where
+        Self: Sized,
+    {
+        match self {
+            FileListing::Selected(name) => Line::raw(&name).bold().render(area, buffer),
+            FileListing::Unselected(name) => Line::raw(&name).render(area, buffer),
+            FileListing::OpenDir(open_dir) => open_dir.render(area, buffer),
+            FileListing::Error => Line::raw("!tf_error!").render(area, buffer),
+        }
+    }
 }
