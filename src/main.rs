8173e7a2f3992d01e9635e503a5bc8256556156e
Jeff <dev@jeffa.io>
2026-07-02 01:13:21 -0400

    Add basic selection

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,9 @@ use std::{error::Error, fs::read_dir, io};
 
 use ratatui::{
     DefaultTerminal, Frame,
-    crossterm::event,
+    crossterm::event::{self, KeyCode},
     layout::{Constraint, Layout},
+    style::{Style, Stylize},
     widgets::{List, ListState},
 };
 
@@ -14,13 +15,21 @@ fn main() -> Result<(), Box<dyn Error>> {
 }
 
 fn run(terminal: &mut DefaultTerminal) -> io::Result<()> {
-    let mut list_state = ListState::default();
+    let mut list_state = ListState::default().with_selected(Some(0));
 
     loop {
         terminal.draw(|frame| render(frame, &mut list_state))?;
 
-        if event::read()?.is_key_press() {
-            break;
+        match event::read()?.as_key_press_event() {
+            Some(key_press) if key_press.modifiers.is_empty() => match key_press.code {
+                KeyCode::Esc | KeyCode::Char('q') => break,
+                KeyCode::Up | KeyCode::Char('k') => list_state.select_previous(),
+                KeyCode::Down | KeyCode::Char('j') => list_state.select_next(),
+                KeyCode::Left | KeyCode::Char('h') => {}
+                KeyCode::Right | KeyCode::Char('l') => {}
+                _ => {}
+            },
+            _ => {}
         }
     }
 
@@ -45,7 +54,7 @@ fn render(frame: &mut Frame, list_state: &mut ListState) {
         },
     );
 
-    let list = List::new(files);
+    let list = List::new(files).highlight_style(Style::default().bold());
 
     frame.render_stateful_widget(list, area, list_state);
 }
