e5ad0db8ee283e964c8933e51fec3f896bf09872
Jeff <dev@jeffa.io>
2026-07-02 06:55:18 -0400

    Simplify and add selection

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::{
     borrow::Cow,
     error::Error,
-    fmt::{Display, Pointer},
+    fmt::{Debug, Display, Pointer},
     fs::{DirEntry, FileType, ReadDir, read_dir},
     io,
 };
@@ -9,12 +9,11 @@ use std::{
 use ratatui::{
     DefaultTerminal, Frame,
     crossterm::event::{self, KeyCode},
-    layout::{Constraint, HorizontalAlignment, Layout},
-    macros::constraints,
+    layout::{Constraint, Layout},
     prelude::*,
     style::Style,
-    text::{Line, Span, Text, ToSpan},
-    widgets::{List, ListItem, ListState, Widget},
+    text::Line,
+    widgets::{ListState, Widget},
 };
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -24,16 +23,16 @@ fn main() -> Result<(), Box<dyn Error>> {
 }
 
 fn run(terminal: &mut DefaultTerminal) -> io::Result<()> {
-    let mut list_state = ListState::default().with_selected(Some(0));
+    let mut app_state = AppState::default();
 
     loop {
-        terminal.draw(|frame| render(frame, &mut list_state))?;
+        terminal.draw(|frame| render(frame, &mut app_state))?;
 
         match event::read()?.as_key_press_event() {
             Some(key_press) if key_press.modifiers.is_empty() => match key_press.code {
                 KeyCode::Esc | KeyCode::Char('q') => break,
-                KeyCode::Up | KeyCode::Char('k') => list_state.select_previous(),
-                KeyCode::Down | KeyCode::Char('j') => list_state.select_next(),
+                KeyCode::Up | KeyCode::Char('k') => app_state.select_previous(),
+                KeyCode::Down | KeyCode::Char('j') => app_state.select_next(),
                 KeyCode::Left | KeyCode::Char('h') => {}
                 KeyCode::Right | KeyCode::Char('l') => {}
                 _ => {}
@@ -45,15 +44,15 @@ fn run(terminal: &mut DefaultTerminal) -> io::Result<()> {
     Ok(())
 }
 
-fn render(frame: &mut Frame, list_state: &mut ListState) {
+fn render(frame: &mut Frame, app_state: &mut AppState) {
     let layout = Layout::vertical([Constraint::Fill(1)]).spacing(1);
     let [area] = frame.area().layout(&layout);
-    let root_files =
-        read_dir(".").map_or_else(|_| OpenDir::empty(), |entries| OpenDir::new(entries));
-    let app = App::new(root_files);
-    let mut app_state = AppState::default();
+    let pwd_files = read_dir(".").expect("Failed to read $PWD");
+    let (pwd, pwd_length) = OpenDir::new(pwd_files);
+    let app = App::new(pwd);
 
-    frame.render_stateful_widget(app, area, &mut app_state);
+    app_state.set_length(pwd_length);
+    frame.render_stateful_widget(app, area, app_state);
 }
 
 struct App {
@@ -70,85 +69,84 @@ impl StatefulWidget for App {
     type State = AppState;
 
     fn render(self, area: Rect, buffer: &mut Buffer, state: &mut Self::State) {
-        self.root.render(area, buffer);
+        self.root.render(area, buffer, state);
     }
 }
 
 #[derive(Default)]
 struct AppState {
+    length: usize,
     selected: Option<usize>,
 }
 
+impl AppState {
+    fn set_length(&mut self, length: usize) {
+        self.length = length;
+    }
+
+    fn select_next(&mut self) {
+        match self.selected {
+            None => self.selected = Some(0),
+            Some(index) if index == self.length - 1 => self.selected = Some(0),
+            Some(index) => {
+                let next = (index + 1).min(self.length - 1);
+
+                self.selected = Some(next);
+            }
+        }
+    }
+
+    fn select_previous(&mut self) {
+        match self.selected {
+            None | Some(0) => self.selected = Some(self.length),
+            Some(index) => self.selected = Some(index - 1),
+        }
+    }
+}
+
 struct OpenDir {
-    files: Vec<FileListing>,
+    files: Vec<String>,
 }
 
 impl OpenDir {
-    fn new(dir: ReadDir) -> Self {
+    fn new(dir: ReadDir) -> (Self, usize) {
         let mut files = Vec::new();
+        let mut count = 0;
 
         for entry in dir {
             let listing = match entry {
-                Ok(entry) => FileListing::new(entry, false),
-                Err(_) => FileListing::Error,
+                Ok(entry) => entry.file_name().to_string_lossy().to_string(),
+                Err(_) => "!tf_error!".to_string(),
             };
 
             files.push(listing);
+            count += 1;
         }
 
-        Self { files }
-    }
-
-    fn empty() -> Self {
-        Self { files: Vec::new() }
+        (Self { files }, count)
     }
 }
 
-impl Widget for OpenDir {
-    fn render(self, area: Rect, buffer: &mut Buffer) {
+impl StatefulWidget for OpenDir {
+    type State = AppState;
+
+    fn render(self, area: Rect, buffer: &mut Buffer, state: &mut Self::State) {
         let constraints = vec![Constraint::Length(1); self.files.len()];
         let areas = area.layout_vec(&Layout::vertical(constraints));
 
-        for (file, area) in self.files.into_iter().zip(areas) {
-            file.render(area, buffer);
-        }
-    }
-}
+        for (index, (file, area)) in self.files.into_iter().zip(areas).enumerate() {
+            let mut line = Line::raw(file);
 
-enum FileListing {
-    Selected(String),
-    Unselected(String),
-    OpenDir(OpenDir),
-    Error,
-}
-
-impl FileListing {
-    fn new(entry: DirEntry, selected: bool) -> Self {
-        match entry.file_type() {
-            Ok(_) => {
-                let name = entry.file_name().to_string_lossy().to_string();
-
-                if selected {
-                    Self::Selected(name)
-                } else {
-                    Self::Unselected(name)
-                }
+            if let AppState {
+                selected: Some(selected),
+                ..
+            } = state
+                && *selected == index
+            {
+                line = line.style(Style::default().bold());
             }
-            _ => Self::Error,
-        }
-    }
-}
 
-impl Widget for FileListing {
-    fn render(self, area: Rect, buffer: &mut Buffer)
-    where
-        Self: Sized,
-    {
-        match self {
-            FileListing::Selected(name) => Line::raw(&name).bold().render(area, buffer),
-            FileListing::Unselected(name) => Line::raw(&name).render(area, buffer),
-            FileListing::OpenDir(open_dir) => open_dir.render(area, buffer),
-            FileListing::Error => Line::raw("!tf_error!").render(area, buffer),
+            line.render(area, buffer);
         }
     }
 }
