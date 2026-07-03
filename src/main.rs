5011da24920e1d8c99eea127a152683bd0d956dc
Jeff <dev@jeffa.io>
2026-07-03 08:27:49 -0400

    Improve selection and expansion

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,9 @@ use std::{
     env,
     error::Error,
     fs::read_dir,
-    hash::{Hash, Hasher},
     io,
     path::PathBuf,
+    time::{Duration, Instant, SystemTime},
 };
 
 use ratatui::{
@@ -13,6 +13,11 @@ use ratatui::{
     layout::{Constraint, Layout},
     prelude::*,
 };
+
+const TARGET_FPS: u64 = 30;
+const TARGET_FRAME_TIME: Duration = Duration::from_millis(1000 / TARGET_FPS);
+const INPUT_INTERVAL: Duration = Duration::from_millis(100);
+const REFRESH_INTERVAL: Duration = Duration::from_millis(100);
 
 fn main() -> Result<(), Box<dyn Error>> {
     let pwd = env::current_dir().expect("I/O Error: Failed to get $PWD variable");
@@ -26,55 +31,74 @@ fn main() -> Result<(), Box<dyn Error>> {
 }
 
 fn run(terminal: &mut DefaultTerminal, app_state: &mut AppState) -> io::Result<()> {
-    loop {
-        terminal.draw(|frame| render(frame, app_state))?;
+    let mut redraw = true;
 
-        match event::read()?.as_key_press_event() {
-            Some(key_press) if key_press.modifiers.is_empty() => match key_press.code {
-                KeyCode::Esc | KeyCode::Char('q') => break,
-                KeyCode::Up | KeyCode::Char('k') => app_state.root.select_previous_child(),
-                KeyCode::Down | KeyCode::Char('j') => app_state.root.select_next_child(),
-                KeyCode::Left | KeyCode::Char('h') => {
-                    if let Some(selected_file) = app_state.root.get_selected_mut() {
-                        selected_file.collapse();
+    loop {
+        if redraw {
+            terminal.draw(|frame| {
+                frame.render_stateful_widget(AppWidget, frame.area(), app_state);
+            })?;
+            redraw = false;
+        }
+
+        if event::poll(INPUT_INTERVAL)? {
+            match event::read()?.as_key_press_event() {
+                Some(key_press) if key_press.modifiers.is_empty() => match key_press.code {
+                    KeyCode::Esc | KeyCode::Char('q') => break,
+                    KeyCode::Up | KeyCode::Char('k') => app_state.root.select_previous_child(),
+                    KeyCode::Down | KeyCode::Char('j') => app_state.root.select_next_child(),
+                    KeyCode::Left | KeyCode::Char('h') => {
+                        app_state.root.collapse_selected();
                     }
-                }
-                KeyCode::Right | KeyCode::Char('l') => {
-                    app_state.root.expand_selected();
-                }
-                KeyCode::Enter | KeyCode::Char(' ') => {
-                    todo!("Handle marking files")
-                }
+                    KeyCode::Right | KeyCode::Char('l') => {
+                        app_state.root.expand_selected();
+                    }
+                    KeyCode::Enter | KeyCode::Char(' ') => {
+                        todo!("Handle marking files")
+                    }
+                    _ => {}
+                },
                 _ => {}
-            },
-            _ => {}
+            }
+
+            redraw = true;
+        }
+
+        if app_state.last_refresh.elapsed() >= REFRESH_INTERVAL {
+            if app_state.refresh() {
+                redraw = true;
+            }
         }
     }
 
     Ok(())
 }
 
-fn render(frame: &mut Frame, app_state: &mut AppState) {
-    let [area] = frame
-        .area()
-        .layout(&Layout::vertical([Constraint::Fill(1)]));
-
-    frame.render_stateful_widget(AppWidget, area, app_state);
-}
-
 #[derive(Debug)]
 struct AppState {
     root: FileState,
+    last_render: Instant,
+    last_refresh: Instant,
 }
 
 impl AppState {
     fn new(pwd_path: PathBuf) -> Self {
+        let now = Instant::now();
         let mut root = FileState::new(pwd_path, 0);
 
         root.selected = true;
         root.expand_selected();
 
-        Self { root }
+        Self {
+            root,
+            last_render: now,
+            last_refresh: now,
+        }
+    }
+
+    fn refresh(&mut self) -> bool {
+        self.last_refresh = Instant::now();
+        self.root.refresh()
     }
 }
 
@@ -83,9 +107,10 @@ struct FileState {
     path: PathBuf,
     dir: bool,
     selected: bool,
-    marked: bool,
     expanded: bool,
+    marked: bool,
     depth: u16,
+    modified: SystemTime,
     children: Vec<FileState>,
     visible_children: u16,
 }
@@ -94,6 +119,7 @@ impl FileState {
     fn new(path: PathBuf, depth: u16) -> Self {
         Self {
             dir: path.is_dir(),
+            modified: path.metadata().unwrap().modified().unwrap(),
             path,
             selected: false,
             marked: false,
@@ -104,56 +130,75 @@ impl FileState {
         }
     }
 
-    fn expand_selected(&mut self) -> bool {
-        if self.selected {
-            self.expand();
-            self.visible_children += self.children.len() as u16;
+    fn read(&mut self) {
+        self.modified = self.path.metadata().unwrap().modified().unwrap();
 
-            return true;
+        let read_path = read_dir(&self.path).expect(&self.error_message());
+
+        for result in read_path {
+            let entry = result.expect(&self.error_message());
+            let listing = FileState::new(entry.path(), self.depth + 1);
+
+            self.children.push(listing);
         }
-
-        let child_count = self.children.len() as u16;
-
-        for child in &mut self.children {
-            if child.expand_selected() {
-                self.visible_children = child_count + child.visible_children;
-
-                return true;
-            }
-        }
-
-        false
     }
 
-    fn expand(&mut self) -> u16 {
-        if self.expanded || !self.path.is_dir() {
-            return 0;
+    fn refresh(&mut self) -> bool {
+        if self.modified == self.path.metadata().unwrap().modified().unwrap() {
+            return false;
         }
 
-        let lines_added = if self.children.is_empty() {
-            let read_path = read_dir(&self.path).expect(&self.error_message());
+        self.children.clear();
+        self.read();
 
-            for result in read_path {
-                let entry = result.expect(&self.error_message());
-                let listing = FileState::new(entry.path(), self.depth + 1);
+        let mut changed = false;
 
-                self.children.push(listing);
+        for child_state in &mut self.children {
+            if child_state.refresh() {
+                changed = true;
+            }
+        }
+
+        changed
+    }
+
+    fn expand_selected(&mut self) {
+        if !self.dir {
+            return;
+        }
+
+        if self.selected {
+            if self.children.is_empty() {
+                self.read();
             }
 
-            self.children.len() as u16
-        } else {
-            let mut total_children = 0;
+            self.visible_children = self.children.len() as u16;
+            self.expanded = true;
 
-            for file_state in &self.children {
-                total_children += file_state.visible_children;
-            }
+            return;
+        }
 
-            total_children
-        };
+        for child_state in &mut self.children {
+            child_state.expand_selected();
+            self.visible_children += child_state.visible_children;
+        }
+    }
 
-        self.expanded = true;
+    fn collapse_selected(&mut self) {
+        if !self.dir {
+            return;
+        }
 
-        lines_added
+        if self.selected {
+            self.visible_children = 0;
+            self.expanded = false;
+
+            return;
+        }
+
+        for child_state in &mut self.children {
+            child_state.collapse_selected();
+        }
     }
 
     fn collapse(&mut self) {
@@ -292,7 +337,16 @@ impl StatefulWidget for AppWidget {
             Constraint::Min(0),
         ]));
 
+        let mut frame_time = state.last_render.elapsed();
+
+        while frame_time < TARGET_FRAME_TIME {
+            frame_time = state.last_render.elapsed();
+        }
+
+        Span::raw(frame_time.as_millis().to_string()).render(title_area, buffer);
         FileWidget.render(child_area, buffer, &mut state.root);
+
+        state.last_render = Instant::now();
     }
 }
 
